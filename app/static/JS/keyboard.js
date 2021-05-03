6de4fb78123a6ff223a41d0c7f35c2cef3bedf1b
Kissa Leon Denis <kldenis2001@gmail.com>
2021-05-03 09:57:46 +0300

    Keyboard Game Initial Code

---
+++ app/static/JS/keyboard.js
@@ -0,0 +1,14 @@
+// var sounds = ["keyboard sounds/316898__jaz-the-man-2__do.wav",
+//               "keyboard sounds/316908__jaz-the-man-2__re.wav",
+//               "keyboard sounds/316906__jaz-the-man-2__mi.wav",
+//               "keyboard sounds/316904__jaz-the-man-2__fa.wav",
+//               "keyboard sounds/316912__jaz-the-man-2__sol.wav",
+//               "keyboard sounds/316902__jaz-the-man-2__la.wav",
+//               "keyboard sounds/316910__jaz-the-man-2__si-stretched.wav",
+//               "keyboard sounds/316900__jaz-the-man-2__do-stretched-octave.wav",
+//               "keyboard sounds/316901__jaz-the-man-2__do-octave.wav"];
+//
+// function playSound(sound_index){
+//   var audio = new Audio(sounds[sound_index]);
+//   audio.play();
+// }
